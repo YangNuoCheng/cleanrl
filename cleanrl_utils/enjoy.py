d3a93002737bd8dff9ef76b25b05a8942f06cb2d
dayyang <18801328722@163.com>
2026-06-24 20:50:31 +0800

    fix(enjoy): re-add --capture_video arg referenced but never defined (#497)

--- cleanrl_utils/enjoy.py
+++ cleanrl_utils/enjoy.py
@@ -20,6 +20,8 @@ def parse_args():
         help="the id of the environment")
     parser.add_argument("--eval-episodes", type=int, default=10,
         help="the number of evaluation episodes")
+    parser.add_argument("--capture_video", action="store_true", default=False,
+        help="whether to capture videos of the agent performances (check out `videos` folder)")
     args = parser.parse_args()
     # fmt: on
     return args
